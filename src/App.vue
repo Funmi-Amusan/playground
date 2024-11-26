976b193ec64c4a8de08a83522e32e2cd2620926f
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-11-26 23:40:49 +0100

    adding on click function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup>
+import JSConfetti from 'js-confetti'
+
+const confetti = new JSConfetti()
+
+function showConfetti() {
+  confetti.addConfetti()
+}
+
+showConfetti()
 </script>
 
 <template>
-<h1>Hellew</h1>
+<h1 @click="showConfetti">🎉 Let's get started!</h1>
 </template>
 
+<style>
+h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+  background-color: bisque;
+  padding: 5px;
+  width:fit-content;
+  justify-self: center;
+}
+</style>
