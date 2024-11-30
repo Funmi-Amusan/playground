c541a573b9a7f272e544d0b3d03fabdc8e78a2f7
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-11-30 22:59:43 +0100

    adding header component

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+            <script setup>
+            import { RouterLink } from 'vue-router';
+            
+            </script>
+
+<template>  
+<header>
+    <nav>
+        <div>
+            <img src="../assets/logo.png" alt="logo">
+            <h1>Todos</h1>
+        </div>
+        <ul>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+        </ul>
+    </nav>
+</header>
+</template>
+
+
+<style scoped>
+nav{
+    display: flex;
+    flex-direction: row;
+    background-color: blanchedalmond;
+    justify-content: space-between;
+}
+div {
+    flex-direction: row;
+    display: flex;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+<h1>hooooooommee</h1>
+</template>
+
+<script setup>
+</script>
