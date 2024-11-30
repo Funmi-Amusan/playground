1795ddd043218fba22434b14030c089193fd6fee
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-11-30 23:24:08 +0100

    create todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,48 @@
+    <script setup>
+    </script>
 <template>
-<h1>hooooooommee</h1>
+  <h1>Create Todos</h1>
+  <div class="input-wrapper">
+      <div class="input-container">
+        <input type="text" placeholder="Enter your task">
+        <button>Create</button>
+      </div>
+  </div>
 </template>
 
-<script setup>
-</script>
+
+<style lang="css" scoped>
+h1{
+    text-align: center;
+    }
+    .input-wrapper{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .input-container{
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+    input{
+        padding: 10px;
+        border-radius: 5px;
+        width: 300px;
+        border: none;
+        outline: none;
+    }
+    button{
+        padding: 10px;
+        border-radius: 5px;
+        border-top-left-radius: 0px;
+        border-bottom-left-radius: 0px;
+        border: none;
+        outline: none;
+        background-color: #007bff;
+        color: #fff;
+        cursor: pointer;
+    }
+    button:hover{
+        background-color: #0069d9;
+    }
+</style>
