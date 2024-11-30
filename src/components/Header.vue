babf098e0911d468c3b0c0e07a33d2905507f7b4
Funmilayo Amusan <funmilayo@healthtracka.com>
2024-11-30 23:05:02 +0100

    styling header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,11 @@
-            <script setup>
-            import { RouterLink } from 'vue-router';
-            
-            </script>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
 
 <template>  
 <header>
     <nav>
-        <div>
+        <div class="logo">
             <img src="../assets/logo.png" alt="logo">
             <h1>Todos</h1>
         </div>
@@ -20,14 +19,43 @@
 
 
 <style scoped>
+header{
+    background-color: gainsboro;
+    padding: 20px;
+}
+.logo {
+    display: flex;
+    align-items: center;
+}
+h1{
+    font-size: 20px;
+    margin-left: 10px;
+}
+img{
+    width: 50px;
+    height: 50px;
+}
 nav{
     display: flex;
+    align-items: center;
     flex-direction: row;
-    background-color: blanchedalmond;
     justify-content: space-between;
 }
 div {
     flex-direction: row;
     display: flex;
+}
+ul{
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+}
+a{
+    text-decoration: none;
+    color: black;
+    margin-left: 20px;
+}
+a:hover{
+    color: rgb(104, 104, 104);
 }
 </style>
